c6744c6d71bd951a6e35692eaa89738a3633eb2a
Galal Abdelaziz <jet2000tasha@hotmail.com>
2024-02-10 23:59:13 +0000

    Weekly Tasks 02 adding new file:   bank.py

---
+++ bank.py
@@ -0,0 +1,9 @@
+# bank.py
+# This program is to sum two amounts(in Cents) entered by the user, and print out the result in Euros.
+# author: Galal Abdelaziz
+
+x = input("Enter amount1(in cent): ")
+y = input("Enter amount2(in cent): ")
+number = int(x) + int(y)
+answer = number /100
+print (f"The sum of these is €{answer}")
