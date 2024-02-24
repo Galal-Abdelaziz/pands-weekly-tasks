6f73b8ea7725cf2238f32ff1fe410c726949c470
Galal Abdelaziz <jet2000tasha@hotmail.com>
2024-02-24 00:40:45 +0000

    Extra comments

--- accounts.py
+++ accounts.py
@@ -1,16 +1,16 @@
-#accounts.py
-#Weekly Tasks 03
-#reads in any length account number and outputs the account number with only the last 4 digits showing.
-#author: Galal Abdelaziz
+# accounts.py
+# Weekly Tasks 03
+# reads in any length account number and outputs the account number with only the last 4 digits showing.
+# author: Galal Abdelaziz
 
 answer = input("Please enter an 10 digit account number:")
 
 '''
-#Can use lines 10-14 (to replace line 16) to restrict input to 10 digits restarts the script until user adds 10 digits using if condistion statment.
+# Can use lines 10-14 (to replace line 16) to restrict input to 10 digits restarts the script until user adds 10 digits using if condition statment.
 if len(answer) != 10:
         print ("Please enter 10 digits")
 if len(answer) == 10:
 
         print ("x" * (len(answer) - 4) + answer[-4:])
 '''
-print ("x" * (len(answer) - 4) + answer[-4:]) # used (-4) to show only the last 4 digits and replace the rest with x's
+print ("x" * (len(answer) - 4) + answer[-4:]) # used len and -4 to replace all of the numbers with x's other than last 4 digits, Used answer[-4:] to show only the last 4 digits.
--- collatz.py
+++ collatz.py
@@ -1,10 +1,16 @@
+# collatz.py
+# Weekly Tasks 04
+# asks the user to input any positive integer and outputs the successive values of the Collatz conjecture.
+# author: Galal Abdelaziz.
+
+
 x = int(input("Please enter a positive number:"))
 
-while x != 1:  #using a while loop to get the whole results.
+while x != 1:  # using a while loop to get the results.
     
-    if x < 1:                                         #using this line to avoid infint loop in the case of negative numbers.
+    if x < 1:                                         # using this line to avoid infint loop in the case of negative numbers.
         print("You can only use a positive number")   # user will get this error if entered negative numbers.
-        break                                         # Stops the loop.
+        break                                         # Stops the loop, to avoid an infinite loop.
         
     elif x % 2 == 0:                                  # Testing for even numbers.
             x = (x // 2)                              # Dividing by 2
